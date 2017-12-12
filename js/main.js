195b1061a716f55529326ff1d187d2c027ab7fc4
Adalaber <maguado85@gmail.com>
2017-12-12 10:09:22 +0100

    for and for and cadena

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 'use strict';
 
 var area = document.querySelector('.text');
-var bucle = '';
+var cadena = '';
 var phrase = 'He aprendido bien como funcionan los bucles';
-for (var i = 0; i < 100; i++) {
-	bucle += '<li>' + phrase + '</li>';
-	}
+var colours = [
+	'pink',
+	'white',
+	'red',
+	'green',
+	'yellow',
+	'blue'
+]
 
-area.innerHTML = bucle;
+// se puede añadir más cadenas en otra línea con ;  y meter un bucle cuando lo nombras en la cadena
+
+for (var i = 0; i < 100; i++) {
+	cadena += '<li>' + phrase + '</li>';
+	cadena += '<select>';
+	for (var j = 0; j < colours.length; j++){
+		cadena += '<option>' + colours[j] + '</option>';
+	}
+	cadena += '</select>';
+}
+
+area.innerHTML = cadena;
